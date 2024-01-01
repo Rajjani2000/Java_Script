b6b42ee5b07e1590ef8607d80e980c9c11471e10
Raj <Rajjani1009@icloud.com>
2024-01-01 14:13:33 -0500

    01/01/24

---
+++ ReactJS/Counter/letscount/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Co from './components/count';
+
+function App() {
+  return (
+    <div className="App">
+    <Co/>  
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
---
+++ ReactJS/Counter/letscount/src/components/count.js
@@ -0,0 +1,58 @@
+import React, {Component} from "react";
+
+class Co extends Component
+{
+    constructor()
+    {
+        super()
+        this.state = 
+        {
+           message: 0
+        }
+    }
+    
+    changeMessage()
+    {
+        this.setState((prevState)=>
+        {
+            return{
+                message: prevState.message+1,
+            };
+        });
+
+    }
+
+    change()
+    {
+        this.setState((again)=>
+        {
+            return{
+                message: again.message-1,
+            };
+        });
+    }
+    
+    
+    render()
+    {
+    return(
+        <div>
+           <h4>{this.state.message}</h4>
+            
+            <button onClick={()=>{this.changeMessage()}}>Count+</button>
+            <button onClick={()=>{this.change()}}>Count-</button>
+        </div>
+    );
+    }
+
+
+
+
+
+}
+
+export default Co
+
+
+
+
